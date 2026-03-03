314231e98ccaf47c5a4ba53fe92005c911aec6f3
cerkiaDev <erickdvid32@gmail.com>
2026-03-03 16:16:14 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_Grupal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
